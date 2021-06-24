749e59805778cd26012507332499b6d08e608b9f
LucasMLara <lucasml@live.com>
2021-06-23 22:34:34 -0300

    sidemenu fixed

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const btnMenu = document.querySelector('.menu-btn');
-btnMenu.addEventListener('click', () => document.querySelector('.menu').classList.toggle('show-menu'));
+btnMenu.addEventListener('click', () => document.querySelector('.side-menu').classList.toggle('show-menu'));
