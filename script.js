3d99a1c25f970966cfe1870eb7cd6044ca6b788a
Lucas Muniz Lara <lucasml@live.com>
2023-05-20 11:56:50 -0300

    iniciando refatoração do menu

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const btnMenu = document.querySelector('.menu-btn');
-btnMenu.addEventListener('click', () => document.querySelector('.side-menu').classList.toggle('show-menu'));
+const btnMenu = document.querySelector(".menu-btn");
+btnMenu.addEventListener("click", () =>
+  document.querySelector(".side-menu").classList.toggle("show-menu")
+);
+
+document.addEventListener("visibilitychange", (e) => {
+  e.preventDefault();
+  if (document.visibilityState == "visible") {
+    document.title = "Lucas M. Lara";
+  } else {
+    document.title = "Volte logo! =D";
+  }
+});
