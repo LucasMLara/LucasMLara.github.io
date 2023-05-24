c99d3736235ca740489e314a7cfa6792148977f4
Lucas Muniz Lara <lucasml@live.com>
2023-05-23 21:57:30 -0300

    function browserdetection

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+const browserName = (function (agent) {
+  switch (true) {
+    case agent.indexOf("edge") > -1:
+      return "MS Edge";
+    case agent.indexOf("edg/") > -1:
+      return "Edge ( chromium based)";
+    case agent.indexOf("opr") > -1 && !!window.opr:
+      return "Opera";
+    case agent.indexOf("chrome") > -1 && !!window.chrome:
+      return "Chrome";
+    case agent.indexOf("trident") > -1:
+      return "MS IE";
+    case agent.indexOf("firefox") > -1:
+      return "Mozilla Firefox";
+    case agent.indexOf("safari") > -1:
+      return "Safari";
+    default:
+      return "other";
+  }
+})(window.navigator.userAgent.toLowerCase());
+
+console.log("You are using " + browserName + " browser");
+
 const menuLink = document.querySelectorAll(".menu-link");
 
-//para fechar o menu com dispositivo pequeno quando clicar em algum link
 menuLink.forEach((link) => {
   link.addEventListener("click", () => {
     if (innerWidth < 750) {
